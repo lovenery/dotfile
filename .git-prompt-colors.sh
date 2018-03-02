5fc9b3efa0562e653e20535a40c4f281be1077fc
Chenxu Wang <edittye379@gmail.com>
2018-03-02 11:31:43 +0800

    update blue color

--- .git-prompt-colors.sh
+++ .git-prompt-colors.sh
@@ -4,7 +4,10 @@
 override_git_prompt_colors() {
   GIT_PROMPT_THEME_NAME="Custom"
 
-  GIT_PROMPT_START_USER="${Yellow}${Time12a}${ResetColor} ${Blue}${PathShort}${ResetColor}"
+  # add user@host
+  #GIT_PROMPT_START_USER="${Yellow}${Time12a}${ResetColor} ${BoldGreen}\u@\h${ResetColor} ${BoldBlue}${PathShort}${ResetColor}"
+  # my default
+  GIT_PROMPT_START_USER="${Yellow}${Time12a}${ResetColor} ${BoldBlue}${PathShort}${ResetColor}"
   GIT_PROMPT_START_ROOT="${GIT_PROMPT_START_USER}"
   GIT_PROMPT_END_USER=" $ "
   GIT_PROMPT_END_ROOT=" # "
