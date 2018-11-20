aaa31de910ca8777b03f85fc9f095108700056c9
Chenxu Wang <cxwangtw@gmail.com>
2018-11-20 18:56:14 +0800

    update

--- .git-prompt-colors.sh
+++ .git-prompt-colors.sh
@@ -5,9 +5,9 @@ override_git_prompt_colors() {
   GIT_PROMPT_THEME_NAME="Custom"
 
   # add user@host
-  #GIT_PROMPT_START_USER="${Yellow}${Time12a}${ResetColor} ${BoldGreen}\u@\h${ResetColor} ${BoldBlue}${PathShort}${ResetColor}"
-  # my default
-  GIT_PROMPT_START_USER="${Yellow}${Time12a}${ResetColor} ${BoldBlue}${PathShort}${ResetColor}"
+  GIT_PROMPT_START_USER="${Yellow}${Time12a}${ResetColor} ${BoldGreen}\u@\h${ResetColor} ${BoldBlue}${PathShort}${ResetColor}"
+  # without user@host
+  #GIT_PROMPT_START_USER="${Yellow}${Time12a}${ResetColor} ${BoldBlue}${PathShort}${ResetColor}"
   GIT_PROMPT_START_ROOT="${GIT_PROMPT_START_USER}"
   GIT_PROMPT_END_USER=" $ "
   GIT_PROMPT_END_ROOT=" # "
--- README.md
+++ README.md
@@ -2,11 +2,13 @@
 
 ```
 cd ~
-git clone git@github.com:lovenery/dotfile.git
+git clone https://github.com/lovenery/dotfile.git
 cp dotfile/.vimrc .
 cp dotfile/.git-prompt-colors.sh .
 rm -rf dotfile
 
 git clone https://github.com/magicmonty/bash-git-prompt.git .bash-git-prompt
+
 source ~/.bash-git-prompt/gitprompt.sh # in .bashrc or .bash_profile
+echo "source ~/.bash-git-prompt/gitprompt.sh" >> .bash_profile
 ```
